5be2d5b2e056ead7a2719a2b9396485e92e2ef3a
SallesCaio <sallescaiosilva@gmail.com>
2025-08-19 22:25:15 -0300

    feat: create a database memory of js

---
+++ database-memory.js
@@ -0,0 +1,30 @@
+export class DatabaseMemory {
+
+    #videos = new Map()
+
+    list() {
+
+        return this.#videos.values()
+        
+    }
+
+    create(video) {
+
+        const videoId = randomUUID()
+
+        this.#videos.set(videoId, video)
+        
+    }
+
+    update(id, video) {
+        this.#videos.set(id, video)
+    }
+
+    delete(id) {
+        this.#videos.set(id)
+    }
+
+}
+
+
+
