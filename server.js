77a112de221943876dba6619d4878bf058c09091
SallesCaio <sallescaiosilva@gmail.com>
2025-08-20 21:13:41 -0300

    feat: create a database conexion and install postgree

--- server.js
+++ server.js
@@ -1,10 +1,78 @@
-import { createServer } from 'node:http';
+//import { createServer } from 'node:http';
 
-const server = createServer((request, response) => {
-    response.write('oi');
+//const server = createServer((request, response) => {
+   // response.write('oi');
 
-    return response.end();
+    //return response.end();
     
+//)
+
+//server.listen(3333)
+
+// GET, 
+
+// POST HTTP:/LOCALHOST:3333/videos
+// PUT HTTP:/LOCALHOST:3333/videos/ID
+
+// Route parameter
+
+// , PUT, DELETE, PATCH
+
+// Request body
+
+import fastify from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
+
+const server = fastify()
+
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body
+
+    database.create({
+        title,
+        description,
+        duration
+    })
+        return reply.status(201).send({
+
+    })
 })
 
-server.listen(3333)
+server.get('/videos', (request) => {
+    const search = request.query.search
+
+    console.log(search)
+
+    const videos = database.list(search)
+
+    return videos                
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    const { title, description, duration } = request.body
+
+    database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+    
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', () => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+
+server.listen({
+    port: 3333,
+})
